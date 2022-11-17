e158bc649432eaf97b84cfc827d0c3af1f668701
arafat-alim <arafat97alim@gmail.com>
2022-11-17 20:14:22 +0530

    👩‍💻 Callback and error handling in asycnhronous code

--- node_example/index.js
+++ node_example/index.js
@@ -7,16 +7,24 @@ const rect = require("./rectangle");
 
 function solveRectangle(l, b) {
   console.log(`THe length is ${l} and breadth is ${b} `);
-  if (l <= 0 || b <= 0) {
-    console.log(
-      `The dimension is not valid, the length is ${l} and breadth is ${b}`
-    );
-    console.log("\n");
-  } else {
-    console.log(`The perimeter of a rectangle is ${rect.perimeter(l, b)}`);
-    console.log(`The area of a rectangle is ${rect.area(l, b)}`);
-    console.log("\n");
-  }
+  // if (l <= 0 || b <= 0) {
+  //   console.log(
+  //     `The dimension is not valid, the length is ${l} and breadth is ${b}`
+  //   );
+  //   console.log("\n");
+  // } else {
+  //   console.log(`The perimeter of a rectangle is ${rect.perimeter(l, b)}`);
+  //   console.log(`The area of a rectangle is ${rect.area(l, b)}`);
+  //   console.log("\n");
+  // }
+  rect(l, b, (err, rectangle) => {
+    if (err) {
+      console.log("Error: ", err.message);
+    } else {
+      console.log(`The perimeter of a rectangle is ${rectangle.perimeter()}`);
+      console.log(`The area of a rectangle is ${rectangle.area()}`);
+    }
+  });
 }
 
 //! Output
--- node_example/rectangle.js
+++ node_example/rectangle.js
@@ -1,2 +1,24 @@
-module.exports.area = (l, b) => l * b;
-module.exports.perimeter = (l, b) => 2 * (l + b);
+// module.exports.area = (l, b) => l * b;
+// module.exports.perimeter = (l, b) => 2 * (l + b);
+
+//! Error handling and call back
+
+module.exports = (x, y, callback) => {
+  if (x <= 0 || y <= 0) {
+    setTimeout(() => {
+      callback(
+        new Error(
+          `The dimension is not valid, the length is ${x} and breadth is ${y} \n`
+        ),
+        null
+      );
+    }, 2000);
+  } else {
+    setTimeout(() => {
+      callback(null, {
+        area: () => x * y,
+        perimeter: () => 2 * (x + y),
+      });
+    }, 2000);
+  }
+};
