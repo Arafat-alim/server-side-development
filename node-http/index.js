2cb3c584df0e25d0b385c2dfc73dea5868bc6a45
arafat-alim <arafat97alim@gmail.com>
2022-11-18 19:50:06 +0530

    Node HTTP, file system and Path core module Example

--- node-http/index.js
+++ node-http/index.js
@@ -1,15 +1,59 @@
 const http = require("http");
+const path = require("path");
+const fs = require("fs");
 const hostname = "localhost";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   //! Req header is coming from the client side
-  console.log(req.headers);
+  //   console.log(req.headers);
+  console.log("Request for " + req.url + " by method " + req.method);
 
-  //! Res - Sending the data to the client
-  res.statusCode = 200;
-  res.setHeader("Content-type", "text/html");
-  res.end("<html><body><h1>Hello, World</h1></body></html>");
+  if (req.method == "GET") {
+    var fileUrl;
+    if (req.url == "/") fileUrl = "/index.html";
+    else fileUrl = req.url;
+
+    var filePath = path.resolve("./public" + fileUrl); // full path of the file
+    //! want to make sure the extension
+    const fileExt = path.extname(filePath);
+    if (fileExt == ".html") {
+      fs.exists(filePath, (exists) => {
+        if (!exists) {
+          res.statusCode = 404;
+          res.setHeader("Content-Type", "text/html");
+          res.end(
+            "<html><body><h1>Error: 404 " +
+              fileUrl +
+              " is not found</h1></body>"
+          );
+          return;
+        } else {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "text/html");
+          fs.createReadStream(filePath).pipe(res);
+        }
+      });
+    } else {
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "text/html");
+      res.end(
+        "<html><body><h1>Error: 404 " + fileUrl + " is not HTML</h1></body>"
+      );
+    }
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/html");
+    res.end(
+      "<html><body><h1>Error: 404 " +
+        req.method +
+        " is not supported</h1></body>"
+    );
+  }
+  //   //! Res - Sending the data to the client
+  //   res.statusCode = 200;
+  //   res.setHeader("Content-type", "text/html");
+  //   res.end("<html><body><h1>Hello, World</h1></body></html>");
 });
 
 server.listen(port, hostname, () => {
