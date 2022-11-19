b8429668a9e95a33ec75f11f772e4bd34ef488f5
arafat-alim <arafat97alim@gmail.com>
2022-11-19 20:39:32 +0530

    Params endpoint added

--- node-express/routes/dishRouter.js
+++ node-express/routes/dishRouter.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const dishRouter = express.Router();
 dishRouter.use(bodyParser.json());
 
+//! Handling the /dishes
 dishRouter
   .route("/")
   .all((req, res, next) => {
@@ -29,6 +30,26 @@ dishRouter
   })
   .delete((req, res, next) => {
     res.end("Delete all the dishes!");
+  });
+
+//! handling the /dishes/:id
+dishRouter
+  .route("/:dishId")
+  .get((req, res, next) => {
+    res.end("Will send the dish: " + req.params.dishId);
+  })
+  .post((req, res, next) => {
+    res.statusCode = 403;
+    res.end("POST operation is not supported on");
+  })
+  .put((req, res, next) => {
+    res.write("Updating the dish: " + req.params.dishId + "\n");
+    res.end(
+      "Update the dish " + req.body.name + " with details " + req.body.details
+    );
+  })
+  .delete((req, res, next) => {
+    res.end("Delete the dish: " + req.params.dishId);
   });
 
 module.exports = dishRouter;
