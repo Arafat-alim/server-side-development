395ce4ad17a485b742edd620b0db15212afbfd12
arafat-alim <arafat97alim@gmail.com>
2022-11-24 17:58:52 +0530

    promise codes.

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,80 +1,47 @@
 const MongoClient = require("mongodb").MongoClient;
-const assert = require("assert");
 const dboper = require("./operations");
 
 const url = "mongodb://127.0.0.1:27017";
 const dbname = "conFusion";
 
 //Connect with the mongodb server
-MongoClient.connect(url, (err, client) => {
-  assert.equal(err, null);
-  console.log("Connected to the mongodb server successfully");
+MongoClient.connect(url)
+  .then((client) => {
+    console.log("Connected correctly to server");
+    const db = client.db(dbname);
 
-  //selecting the database
-  const db = client.db(dbname);
+    dboper
+      .insertDocument(db, { name: "Vadonut", description: "Test" }, "dishes")
+      .then((result) => {
+        console.log("Insert Document:\n", result.ops);
 
-  //selecting the collection from the db
-  // const collection = db.collection("dishes");
-
-  dboper.insertDocument(
-    db,
-    { name: "Vadonut", description: "Test" },
-    "dishes",
-    (result) => {
-      console.log("Insert Document:\n", result.ops);
-
-      dboper.findDocuments(db, "dishes", (docs) => {
+        return dboper.findDocuments(db, "dishes");
+      })
+      .then((docs) => {
         console.log("Found Documents:\n", docs);
 
-        dboper.updateDocument(
+        return dboper.updateDocument(
           db,
           { name: "Vadonut" },
           { description: "Updated Test" },
-          "dishes",
-          (result) => {
-            console.log("Updated Document:\n", result.result);
-
-            dboper.findDocuments(db, "dishes", (docs) => {
-              console.log("Found Updated Documents:\n", docs);
-
-              db.dropCollection("dishes", (result) => {
-                console.log("Dropped Collection: ", result);
-
-                client.close();
-              });
-            });
-          }
+          "dishes"
         );
-      });
-    }
-  );
-  // //Inserting the single docs in to the colleciton
-  // collection.insertOne(
-  //   { name: "Murghmussalam", description: "testing1" },
-  //   (err, result) => {
-  //     assert.equal(err, null);
+      })
+      .then((result) => {
+        console.log("Updated Document:\n", result.result);
 
-  //     console.log("After insert: \n");
-  //     console.log(result.ops); //displaying the inserted docs
-  //     //ops --> tells us how many operation is performed till now
+        return dboper.findDocuments(db, "dishes");
+      })
+      .then((docs) => {
+        console.log("Found Updated Documents:\n", docs);
 
-  //     //Finding the doc from the collection - READ operation
-  //     //! toArray() method convert the object into an array
+        return db.dropCollection("dishes");
+      })
+      .then((result) => {
+        console.log("Dropped Collection: ", result);
 
-  //     collection.find({}).toArray((err, docs) => {
-  //       assert.equal(err, null);
-
-  //       //print the array
-  //       console.log("Found: \n");
-  //       console.log(docs);
-
-  //       //Now Delete the collection from the database
-  //       db.dropCollection("dishes", (err, result) => {
-  //         assert.equal(err, null);
-
-  //         client.close(); //close the connection
-  //       });
-  //     });
-  //   }
-  // );
-});
+        return client.close();
+      })
+      .catch((err) => console.log(err));
+  })
+  .catch((err) => console.log(err));
