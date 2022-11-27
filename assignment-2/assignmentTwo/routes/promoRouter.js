f6cd964da0a01c5c17d28907a7378b60d89c08eb
arafat-alim <arafat97alim@gmail.com>
2022-11-27 14:25:02 +0530

    🌍Assignment 2 - Promo Router REST API successfully completed.

--- assignment-2/assignmentTwo/routes/promoRouter.js
+++ assignment-2/assignmentTwo/routes/promoRouter.js
@@ -36,11 +36,64 @@ promoRouter
     res.end(`PUT operation is not supported on this /promotions`);
   })
   .delete((req, res, next) => {
-    Promos.remove({}).then((promos) => {
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "application/json");
-      res.json(promos);
-    });
+    Promos.remove({})
+      .then(
+        (promos) => {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(promos);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  });
+promoRouter
+  .route("/:promoId")
+  .get((req, res, next) => {
+    Promos.findById(req.params.promoId)
+      .then(
+        (promo) => {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(promo);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  })
+  .post((req, res, next) => {
+    res.statusCode = 403;
+    res.end(
+      `POST operation is not supported on /promotions/${req.params.promoId}`
+    );
+  })
+  .put((req, res, next) => {
+    Promos.findByIdAndUpdate(
+      req.params.promoId,
+      { $set: req.body },
+      { new: true }
+    )
+      .then(
+        (promo) => {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(promo);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  })
+  .delete((req, res, next) => {
+    Promos.findByIdAndDelete(req.params.promoId)
+      .then(
+        (promo) => {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(promo);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
   });
 
 module.exports = promoRouter;
