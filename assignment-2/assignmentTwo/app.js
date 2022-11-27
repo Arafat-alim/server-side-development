5cddbc42affb510b78a383c18881491376bd667a
arafat-alim <arafat97alim@gmail.com>
2022-11-27 16:57:07 +0530

    🌍 Assignment 2 - Leader REST api router Task 2 completed

--- assignment-2/assignmentTwo/app.js
+++ assignment-2/assignmentTwo/app.js
@@ -14,7 +14,7 @@ connect.then((db) => {
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const promoRouter = require("./routes/promoRouter");
-
+const leaderRouter = require("./routes/leaderRouter");
 var app = express();
 
 // view engine setup
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/promotions", promoRouter);
-
+app.use("/leaders", leaderRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
