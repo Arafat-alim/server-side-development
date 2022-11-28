956b86e052171122a68029fd8f40b00bcc46d31c
arafat-alim <arafat97alim@gmail.com>
2022-11-28 20:39:45 +0530

    Added the restful api full featured

---
+++ node-authentication-one/restAuth/models/dishes.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//! Comment Schema
+const commentSchema = new Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//! Dish Schema
+const dishSchema = new Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+    comments: [commentSchema], //! subdocument in mongoose
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Dishes = mongoose.model("Dish", dishSchema);
+
+module.exports = Dishes;
---
+++ node-authentication-one/restAuth/routes/promoRouter.js
@@ -0,0 +1,99 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const promoRouter = express.Router();
+const Promos = require("../models/promotions");
+
+promoRouter.use(bodyParser.json());
+
+promoRouter
+  .route("/")
+  .get((req, res, next) => {
+    Promos.find()
+      .then(
+        (promos) => {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(promos);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  })
+  .post((req, res, next) => {
+    Promos.create(req.body)
+      .then(
+        (promo) => {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(promo);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  })
+  .put((req, res, next) => {
+    res.statusCode = 403;
+    res.end(`PUT operation is not supported on this /promotions`);
+  })
+  .delete((req, res, next) => {
+    Promos.remove({})
+      .then(
+        (promos) => {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(promos);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  });
+promoRouter
+  .route("/:promoId")
+  .get((req, res, next) => {
+    Promos.findById(req.params.promoId)
+      .then(
+        (promo) => {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(promo);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  })
+  .post((req, res, next) => {
+    res.statusCode = 403;
+    res.end(
+      `POST operation is not supported on /promotions/${req.params.promoId}`
+    );
+  })
+  .put((req, res, next) => {
+    Promos.findByIdAndUpdate(
+      req.params.promoId,
+      { $set: req.body },
+      { new: true }
+    )
+      .then(
+        (promo) => {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(promo);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  })
+  .delete((req, res, next) => {
+    Promos.findByIdAndDelete(req.params.promoId)
+      .then(
+        (promo) => {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(promo);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  });
+
+module.exports = promoRouter;
