3ccd8814d3a081ef5144c2291d6eeb7508a1aec1
Arafat Alim <arafat97alim@gmail.com>
2022-12-05 20:56:45 +0530

    4️⃣ User model is ready to Go!

---
+++ assignment-3/conFusion/models/users.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+//! Adding plugin
+const pasportLocalMongoose = require("passport-local-mongoose");
+
+const User = new Schema({
+  firstname: {
+    type: String,
+    default: "",
+  },
+  lastname: {
+    type: String,
+    defaullt: "",
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+User.plugin(pasportLocalMongoose);
+
+module.exports = mongoose.model("User", User);
